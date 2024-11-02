a31e33db2682c535c8bbed858118c6d9ccc0690c
Y21-proyect <frangp.inv@gmail.com>
2024-11-02 02:27:09 +0100

    Update Elsa explica.py

--- Elsa explica.py
+++ Elsa explica.py
@@ -30,34 +30,146 @@ mitad_letras1=0
 numero_letras=0
 avanzar=''
 
+def clear_console(): #FUNCION LIMPIAR CONSOLA
+    if os.name == 'nt': # selector de sistema operativo para borrar el texto de la consola
+        os.system('cls')     # Comando para Windows
+        
+    else:
+        os.system('clear')   # Comando para Unix/Linux/Mac
+
+    return()
+
+def codg70(num,let,act):
+      
+      abc='a,b,c,d,e,f,g,h,i,j,k,l,ll,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
+      ABC='A,B,C,D,E,F,G,H,I,J,K,L,LL,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z'
+      NUM='0,1,2,3,4,5,6,7,8,9'
+      SIMB= '¡,!,(,),[,],¬'
+
+      cod=abc+','+ABC+','+NUM+','+SIMB
+      cod70=cod.split(',')
+
+      n=''
+      s=0
+
+      
+      #codificado a  base 70
+
+      if act==1:
+            for n in cod70:
+                  if s==num:
+                        return(n)
+                  s+=1
+
+      if act==2:
+            for n in cod70:
+                  if n==let:
+                        return(s)
+                  s+=1
 
 
-a='\n\n'
-b=   '                 yyyy          yyyy   '
-c=   '                  yyyy        yyyy    '
-c=   '                   yyyy      yyyy     ' 
-d=   '                    yyyy    yyyy      ' 
-e=   '                     yyyy  yyyy       ' 
-f=   '                      yyyyyyyy        ' 
-g=   '                        yyyy          ' 
-h =  '                        yyyy          ' 
-i =  '                        yyyy          ' 
-j =  '                        yyyy    222       1   ' 
-k =  '                        yyyy   2   2    1 1   ' 
-l=   '                        yyyy      2       1   ' 
-ll=  '                        yyyy    2         1   '  
-m=   '                        yyyy   22222    11111 \n\n '
-n=   '                    (Yogalidof 21)\n\n\n'
+def db70ab10(letrs):
+      num=0
+      l=0
+      n=''
+      m=''
+      o=0
+      p=0
+      q=0
+      llave=0
 
-o=(a,b,c,d,e,f,g,h,i,j,k,l,ll,m,n)
+      for n in letrs:  # proceso se hace escalonado de abajo hacia arriba, la llave 0
+                   # multiplica el cociente por 5, la llave 1 le suma el ultimo cociente
+                   # la llave 2 suma cocientes anteriores y multiplica por 70        
 
-for p in o:
-    print(p)
-    time.sleep(0.07)
+            l=int(codg70(num,n,2)) # cambio de codigo base 70 a base 10
+            
+            if llave==2:
+                  p=l+(p*70) # multiplica por 70 operacion y suma resto hasta final de interación
 
-time.sleep(1)
+            if llave==1: # suma el primer resto al proceso de la llave 0 durante la segunda iteracion
+                  p= l+p
+                  llave=2
 
-os.system('cls')
+            if llave==0: # multiplicar ultimo cociente por 70
+                         # en la primera iteración
+                  p=l*70
+                  llave=1
+
+              
+      n=str(p)  
+
+      return(n)            
+            
+
+def db10ab5(num):
+      cif=int(num)
+      llave=True
+      n=''
+      m=''
+      o=''
+      p=0
+      
+      while llave==True:
+            p=cif%5
+            cif=cif//5
+            m=str(p)+m
+            if cif<5:
+                  m=str(cif)+m
+                  llave=False
+
+      return(m)
+
+
+
+def db70ab5(num):
+      
+      num=db70ab10(num)
+      return(db10ab5(num))
+      
+
+def animacion():   
+
+      p=0
+      q=0
+      u=12
+      s=''
+      r=[]
+      
+      dib=('ak]7!u!2U','azjyyXii','afmU1J4i','bcQEEii','bcQnAPD','c9yO3W','c9yO3W','c9yO3W','akLKfE]ayQRb','akLKfE]dDm0S','akLKfE[]AuGN','akLKfE]al[aX','dZZp(CSNajN7')
+
+      while p<13:
+            if p==13:
+                  break
+            clear_console()
+            r.append(p) # crea la figura que se crea en cada iteración
+                        # dentro de la lista r
+            q=12-p
+            p=p+1
+
+            print('\n\n\n')
+
+            while q>0:
+                  print('')
+                  q=q-1
+                  
+            for s in r:
+                  s=db70ab5(dib[s])
+                  s=s.replace('4','').replace('3','y').replace('0',' ')
+                  print('              '+s)
+
+
+            time.sleep(0.000000007)
+
+      time.sleep(0.7)
+      
+      print('\n\n               (Yogalidof 21 Proyect)')
+
+      time.sleep(1)
+
+      clear_console()
+
+animacion()
 
 print ('\n\n    Este programa es parte de un trabajo altruista y tambien de retos que me propongo')
 print ('    La parte altruista és que facilita la vida a otras personas.')
@@ -205,6 +317,9 @@ while llave2 == True:
         puntero+=1 # incrementa en 1 el valor del puntero que localiza la línea a imprimir
 
         if avanzar=='1': # abre la llave 2 y se elige otra página
+            avanzar=''
+            llave3=True
+            os.system('cls')
             avanzar=''
             llave3=True
             os.system('cls')
